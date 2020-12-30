5c596f28e2c8f7c39b1942c7ecb24f1480490295
mauger <auger.mickael37@gmail.com>
2020-12-30 11:41:57 +0100

    [SECTION_8][COURS_44] Gestion de la commande (transporteurs & adresses)

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Entity\Address;
 use App\Form\AddressType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -31,7 +32,7 @@ class AccountAddressController extends AbstractController
     /**
      * @Route("/compte/ajouter-adresse", name="account_address_add")
      */
-    public function add(Request $request)
+    public function add(Cart $cart, Request $request)
     {
         $address = new Address();
         $form    = $this->createForm(AddressType::class, $address);
@@ -42,6 +43,11 @@ class AccountAddressController extends AbstractController
             $address->setUser($this->getUser());
             $this->entityManager->persist($address);
             $this->entityManager->flush();
+
+
+            if ($cart->get()) {
+                return $this->redirectToRoute('order');
+            }
 
             return $this->redirectToRoute('account_address');
         }
---
+++ src/Controller/OrderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\OrderType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @Route("/commande", name="order")
+     */
+    public function index()
+    {
+        if (!$this->getUser()->getAddresses()->getValues()) {
+            return $this->redirectToRoute('account_address_add');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
