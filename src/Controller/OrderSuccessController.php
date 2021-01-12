dfa9063cbfdad461eae5172c19d8954d85c62e11
mauger <mickael.auger@apside-groupe.com>
2021-01-12 08:31:10 +0100

    [SECTION_10][COURS_59] Envoyer des mails au client

--- src/Controller/OrderSuccessController.php
+++ src/Controller/OrderSuccessController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Classe\Mail;
 use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,7 +34,11 @@ class OrderSuccessController extends AbstractController
             $cart->remove();
             $order->setIsPaid(1);
             $this->entityManager->flush();
-            // todo : envoyer un mail
+
+            $mail    = new Mail();
+            $content = 'Bonjour' . $order->getUser()->getFirstname() . '<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid consequatur dicta dolore eius eos, error, fugiat iste magnam maiores maxime mollitia, nesciunt nostrum porro similique sit vel voluptates voluptatum?';
+
+            $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstname(), 'Votre commande La Boutique Française est bien validée !',$content);
         }
 
         return $this->render('order_success/index.html.twig', [
