a5a8bd6e4f6122059cbf6ebdee4a2cdde523543c
mauger <mickael.auger@apside-groupe.com>
2021-01-25 09:18:31 +0100

    [FINISH]

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/nous-contacter", name="contact")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('notice', 'Merci de nous avoir contacté. Notre équipe vous répondra dans les plus brefs délais !');
+
+            // ajouter l'envoi à l'email.
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
